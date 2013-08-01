c0a49bc18b5e2079d6e07986e7fc1fe1e2d8c65a
Abhinav Manchanda <abhinav.manchanda@gmail.com>
2013-08-01 17:39:55 +0530

    Abhinav: Introducing additional layers.

--- src/com/example/ShoppingApplication/adapters/ShoppingItemsListingAdapter.java
+++ src/com/example/ShoppingApplication/adapters/ShoppingItemsListingAdapter.java
@@ -3,11 +3,15 @@ package com.example.ShoppingApplication.adapters;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.GridView;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+import com.example.ShoppingApplication.R;
 import com.example.ShoppingApplication.services.ImageDownloader;
 
 import java.io.IOException;
@@ -49,15 +53,20 @@ public class ShoppingItemsListingAdapter extends BaseAdapter {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        ImageView imageView = new ImageView(mContext);
+//        ImageView imageView = new ImageView(mContext);
         ImageDownloader imageDownlaoder = new ImageDownloader();
         Bitmap bitmap = imageDownlaoder.downloadImage(mThumbIds.get(position));
-
+//
+//        imageView.setImageBitmap(bitmap);
+//
+//        imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+//        imageView.setLayoutParams(new GridView.LayoutParams(70, 70));
+//        return imageView;
+        LinearLayout layout = (LinearLayout) LayoutInflater.from(mContext).inflate(R.layout.cell_layout, null);
+        ImageView imageView = (ImageView) layout.findViewById(R.id.imageView);
         imageView.setImageBitmap(bitmap);
+        return layout;
 
-        imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
-        imageView.setLayoutParams(new GridView.LayoutParams(70, 70));
-        return imageView;
     }
 }
 
